dbebd185925397c13195c1afb4e0c3ea1a6074c4
Juan <104juanjose@gmail.com>
2026-04-29 21:44:18 -0300

    fix puerto render

--- server.js
+++ server.js
@@ -79,6 +79,8 @@ app.get('/historial/:user_id', (req, res) => {
 });
 
 // INICIAR SERVIDOR
-app.listen(3000, () => {
-  console.log('Servidor en http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo");
 });
